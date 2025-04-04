6e9cb6a4e3b43a6c6d52a201bbf08c2ef78a5f30
jorgeMontoya022 <142169854+jorgeMontoya022@users.noreply.github.com>
2025-04-04 13:59:33 -0500

    remove

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module co.edu.uniquindio.task.task_app {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens co.edu.uniquindio.task.task_app to javafx.fxml;
-    exports co.edu.uniquindio.task.task_app;
-}
